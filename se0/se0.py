983640de42a13bc8113d55d7afe24ae08cae3f27
Hannah Morgan <hannah.mairs@gmail.com>
2023-09-22 10:20:28 -0500

    Adding SE0

---
+++ se0/se0.py
@@ -0,0 +1,14 @@
+"""
+Short Exericses #0
+"""
+
+def two_plus_two():
+    """ 
+    Calculates the result of 2+2
+    """
+
+    ### REPLACE 0 WITH 2+2:
+    return 0
+
+if __name__=="__main__":
+    print("2+2 is", two_plus_two())
---
+++ se0/test_se0.py
@@ -0,0 +1,81 @@
+import sys
+import os
+
+# Handle the fact that the test code may not
+# be in the same directory as the solution code
+sys.path.insert(0, os.getcwd())
+
+import se0
+
+MODULE = "se0"
+
+def test_two_plus_two():
+    recreate_msg = gen_recreate_msg(MODULE, "two_plus_two", *())
+
+    actual = se0.two_plus_two()
+
+    check_none(actual, recreate_msg)
+    check_type(actual, 4, recreate_msg)
+    check_equals(actual, 4, recreate_msg)
+
+# # #
+#
+# HELPER FUNCTIONS
+#
+# # #
+
+def gen_recreate_msg(module, function, *params):
+    params_str = ", ".join([str(p) for p in params])
+
+    recreate_msg = "To recreate this test in ipython3 run:\n"
+    recreate_msg += "  {}.{}({})".format(module, function, params_str)
+
+    return recreate_msg
+
+
+def check_none(actual, recreate_msg=None):
+    msg = "The function returned None."
+    msg += " Did you forget to replace the placeholder value we provide?"
+    if recreate_msg is not None:
+        msg += "\n" + recreate_msg
+
+    assert actual is not None, msg
+
+
+def check_type(actual, expected, recreate_msg=None):
+    actual_type = type(actual)
+    expected_type = type(expected)
+
+    msg = "The function returned a value of the wrong type.\n"
+    msg += "  Expected return type: {}.\n".format(expected_type.__name__)
+    msg += "  Actual return type: {}.".format(actual_type.__name__)
+    if recreate_msg is not None:
+        msg += "\n" + recreate_msg
+
+    assert isinstance(actual, expected_type), msg
+
+
+def check_equals(actual, expected, recreate_msg=None):
+    msg = "Actual ({}) and expected ({}) values do not match.".format(actual, expected)
+    if recreate_msg is not None:
+        msg += "\n" + recreate_msg
+
+    assert actual == expected, msg
+
+
+def check_float_equals(actual, expected, epsilon, recreate_msg=None):
+    msg = "Actual ({}) and expected ({}) values do not match.".format(actual, expected)
+    if recreate_msg is not None:
+        msg += "\n" + recreate_msg
+
+    assert abs(actual - expected) < epsilon, msg
+
+
+def check_list_unmodified(param_name, before, after, recreate_msg=None):
+    msg = "You modified the contents of {} (this is not allowed).\n".format(param_name)
+    msg += "  Value before your code: {}\n".format(before)
+    msg += "  Value after your code:  {}".format(after)
+    if recreate_msg is not None:
+        msg += "\n" + recreate_msg
+
+    assert before == after, msg
