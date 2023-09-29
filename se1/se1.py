0c0b94bda5f3eb398f36ccb8690d5cd591535a6c
Hannah Morgan <hannah.mairs@gmail.com>
2023-09-29 16:25:58 -0500

    Adding SE1

---
+++ se1/se1.py
@@ -0,0 +1,94 @@
+"""
+Short Exercises #1
+"""
+
+def add_one_and_multiply(a, x):
+    """ 
+    Add 1 to a, and multiply by x.
+    """
+
+    ### EXERCISE 1 -- YOUR CODE GOES HERE
+    # Replace "None" with the correct expression.
+    r = None
+
+    ### DO NOT MODIFY THE FOLLOWING LINE!
+    return r
+
+
+def same_parity(a, b):
+    """ 
+    Determine whether or not a and b have the same parity.
+    """
+
+    ### EXERCISE 2 -- YOUR CODE GOES HERE
+    # Replace "None" with the correct expression.
+    rv = None
+
+    ### DO NOT MODIFY THE FOLLOWING LINE!
+    return rv
+
+
+def num_to_string(x):
+    """
+    Given a number x, produce a string "POSITIVE", "NEGATIVE", "ZERO"
+    (depending on whether the number is positive, negative, or zero).
+    """
+
+    ### EXERCISE 3 -- YOUR CODE GOES HERE
+    # After running your code, variable s should contain the value
+    # we ask you to compute in this exercise.
+    # You may use more variables if you wish.
+    s = None
+
+    ### DO NOT MODIFY THE FOLLOWING LINE!
+    return s
+
+
+def num_divisible(lb, ub, p, q):
+    """
+    Determine how many numbers between lb and ub (inclusive)
+    are divisible by p or divisible by q, but not divisible 
+    by p and q.
+    """
+
+    ### EXERCISE 4 -- YOUR CODE GOES HERE
+    # After running your code, variable n should contain the value
+    # we ask you to compute in this exercise.
+    # You may use more variables if you wish.
+    n = None
+
+    ### DO NOT MODIFY THE FOLLOWING LINE!
+    return n
+
+
+def num_greater_than(lst, t):
+    """
+    Given a list and a target value, determine the number of values
+    in the list strictly greater than the target.
+    """
+
+    ### EXERCISE 5 -- YOUR CODE GOES HERE
+    # After running your code, variable n should contain the value
+    # we ask you to compute in this exercise.
+    # You may use more variables if you wish.
+    n = None
+
+    ### DO NOT MODIFY THE FOLLOWING LINE!
+    return n
+
+
+def keep_if_in_range(lst, lb, ub):
+    """
+    Given a list, a lower bound, and an upper bound, create a new 
+    list that contains the values in the input list that are
+    between lb and ub (inclusive).
+    """
+
+    ### EXERCISE 6 -- YOUR CODE GOES HERE
+    # After running your code, variable new_lst should contain the value
+    # we ask you to compute in this exercise.
+    # You may use more variables if you wish.
+    new_lst = None
+
+    ### DO NOT MODIFY THE FOLLOWING LINE!
+    return new_lst
